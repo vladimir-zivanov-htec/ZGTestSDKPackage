5f609d0284a0ca189cddf8f94b5617083cdeb728
Vladimir Zivanov <vladimir.zivanov@htecgroup.com>
2023-08-04 18:18:17 +0200

    Test

--- Package.swift
+++ Package.swift
@@ -11,9 +11,6 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ZGTestSDKPackage",
-            targets: ["ZGTestSDKPackage"]),
-        .library(
             name: "ZGTestSDK",
             targets: ["ZGTestSDK"]),
     ],
@@ -24,14 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ZGTestSDKPackage",
-            dependencies: []),
-        .testTarget(
-            name: "ZGTestSDKPackageTests",
-            dependencies: ["ZGTestSDKPackage"]),
         .binaryTarget(
-                    name: "ZGTestSDK",
-                    path: "./Sources/ZGTestSDK.xcframework")
+            name: "ZGTestSDK",
+            path: "./Sources/ZGTestSDK.xcframework")
     ]
 )
--- Sources/ZGTestSDKPackage/ZGTestSDKPackage.swift
+++
@@ -1,6 +0,0 @@
-public struct ZGTestSDKPackage {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
