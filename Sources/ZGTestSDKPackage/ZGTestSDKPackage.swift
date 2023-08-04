8362fa41f5e2dc7d3544887e161443798ee98f6d
Vladimir Zivanov <vladimir.zivanov@htecgroup.com>
2023-08-04 17:54:29 +0200

    Add package

---
+++ Sources/ZGTestSDKPackage/ZGTestSDKPackage.swift
@@ -0,0 +1,6 @@
+public struct ZGTestSDKPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ZGTestSDKPackageTests/ZGTestSDKPackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ZGTestSDKPackage
+
+final class ZGTestSDKPackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ZGTestSDKPackage().text, "Hello, World!")
+    }
+}
