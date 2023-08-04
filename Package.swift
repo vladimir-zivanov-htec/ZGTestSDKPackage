992bbc6866d6e82931f21a7fe8d59ab59a58fb4a
Vladimir Zivanov <vladimir.zivanov@htecgroup.com>
2023-08-04 19:52:11 +0200

    FirebaseAppCheck dependency added

--- Package.swift
+++ Package.swift
@@ -17,12 +17,23 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk.git",
+            .upToNextMajor(from: "10.4.0")
+          ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "ZGTestSDK",
-            path: "./Sources/ZGTestSDK.xcframework")
+            path: "./Sources/ZGTestSDK.xcframework"),
+        .target(
+          name: "ZGTestSDK",
+          dependencies: [
+            // The product(s) you want (e.g. FirebaseAuth).
+            .product(name: "FirebaseAppCheck", package: "Firebase"),
+          ]
+        ),
     ]
 )
